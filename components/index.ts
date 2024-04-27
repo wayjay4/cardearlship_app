6dad947b4820d8efea86e778ad4632ddcadd7fc5
Waylon Dixon <wd@signingdaysports.com>
2024-04-27 11:23:48 -0700

    added public images for site, removed components/index.ts as it was not needed

--- components/index.ts
+++
@@ -1,9 +0,0 @@
-// a single file to export all our components,
-// so we can import them simpler in the pages where we use them
-// NOTE: I believe this is for VisualBasic, PHPStorm does this automatically
-
-import Hero from './Hero';
-
-export {
-    Hero
-};
